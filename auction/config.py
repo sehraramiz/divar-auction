f988f9aa524e4156ccd116ac044abdafb55b74ca
Mohsen Barzegar <mohsenbarzegar@disroot.org>
2024-11-29 12:21:14 +0330

    build: add debug to config

--- auction/config.py
+++ auction/config.py
@@ -7,7 +7,16 @@ class DivarConfig(BaseSettings):
     oauth_secret: str
     oauth_redirect_url: str
 
-    model_config = SettingsConfigDict(env_file=".env", env_prefix="divar_")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_prefix="divar_", extra="allow"
+    )
 
 
+class Config(BaseSettings):
+    debug: bool = False
+
+    model_config = SettingsConfigDict(env_file=".env", extra="allow")
+
+
+config = Config()  # type: ignore
 divar_config = DivarConfig()  # type: ignore
